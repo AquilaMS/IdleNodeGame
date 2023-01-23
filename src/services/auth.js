6cb04734cdf2b4ece4e172c379110b8fdd76ccaa
AquilaMS <aquila.botafogo@gmail.com>
2023-01-23 16:39:48 -0300

    modified:   src/services/auth.js 	modified:   tests/auth.test.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app.js
    	deleted:    src/error/erros.js
    	modified:   src/routes/auth.js
    	modified:   src/routes/users.js
    	modified:   src/services/powerups.js
    	modified:   src/services/users.js
    	modified:   tests/users.test.js
    	tests/fakeusers.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,16 +1,54 @@
 var db = require('../../knexfile')
+const bcrypt = require('bcrypt')
+const SECRET = 'super hyper secret'
+const jwt = require('jwt-simple')
 
+const getPasswordHash = (pwd) => {
+  const salt = bcrypt.genSaltSync(10)
+  return bcrypt.hashSync(pwd, salt)
+}
 const createUser = async (req) => {
-  const aaa = await db.insert(req).into('users').then(result => {
-    console.log(result)
+  if (!req.name) return { error: 'Insert a name' }
+  if (!req.email) return { error: 'Insert a email' }
+  if (!req.password) return { error: 'Insert a password' }
+
+  req.password = getPasswordHash(req.password)
+  const newUser = await db.insert(req).into('users').then(result => {
   }).catch(err => {
     return { error: 'Invalid' }
   })
-  return aaa
+  return { newUser }
 }
 
+const getUserData = async (filter = {}) => {
+  const res = await db.select().table('users').where(filter).first()
+  //console.log(res)
+  return res
+}
 
-
+const signIn = async (req) => {
+  return getUserData({ email: req.email })
+    .then(user => {
+      console.log(user)
+      console.log(req)
+      if (bcrypt.compareSync(req.password, user.password)) {
+        const payload = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        }
+        const token = jwt.encode(payload, SECRET)
+        return token
+      }
+      else {
+        return { error: 'Invalid user' }
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+}
 module.exports = {
   createUser,
+  getUserData,
+  signIn
 }
--- tests/auth.test.js
+++ tests/auth.test.js
@@ -2,22 +2,54 @@ const request = require('supertest')
 const app = require('../src/app')
 var db = require('../knexfile')
 const MAIN_ROUTE = '/acc'
-
-let fake_user1 = {
-  name: 'testName',
-  email: 'test@test.com',
-  password: '123',
-  balance: 0,
-  outDate: new Date('2021-03-25'),
-  inDate: new Date('2021-03-26'),
-  sumMultiplier: 2
-}
+const fakeUsers = require('./fakeusers')
+const authServices = require('../src/services/auth')
 
 
 test('email must be unique', () => {
   return request(app)
-    .post(`${MAIN_ROUTE}/signup`)
-    .send(fake_user1)
+    .post(`${MAIN_ROUTE}/get`)
+    .send({ id: 44 })
     .then(res => {
+      expect(res.status).toBe(200)
     })
 })
+
+test('check fields', () => {
+  return request(app)
+    .post(`${MAIN_ROUTE}/signup`)
+    .send({})
+    .then(res => {
+      expect(res.body).toHaveProperty('error')
+    })
+})
+
+test('insert a user', () => {
+  return request(app)
+    .post(`${MAIN_ROUTE}/signup`)
+    .send(fakeUsers.fake_user3)
+    .then(res => {
+      expect(res.status).toBe(201)
+    })
+})
+
+test('get bad password', () => {
+  return authServices.createUser(fakeUsers.fake_user3)
+    .then(() => request(app).post('/acc/signin')
+      .send({ email: fakeUsers.fake_user3.email, password: '1234' }))
+    .then(res => {
+      expect(res.status).toBe(300)
+      expect(res.body).toHaveProperty('error')
+    })
+})
+
+test('get good password', () => {
+  return authServices.createUser(fakeUsers.fake_user3)
+    .then(() => request(app).post('/acc/signin')
+      .send({ email: fakeUsers.fake_user3.email, password: '123' }))
+    .then(res => {
+      expect(res.status).toBe(201)
+      expect(res.body).toHaveProperty('token')
+    })
+})
+
